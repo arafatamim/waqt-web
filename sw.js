73fee79c3bc111d181e7b66fcdd8cafc176f717e
Tamim Arafat <tamim.arafat@gmail.com>
2018-06-11 20:55:38 +0300

    Minor changes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
 importScripts('sw-toolbox.js');
 toolbox.precache(["index.html", "styles/style.css", "scripts/script.js"]);
-toolbox.router.get('/assets/*', toolbox.cacheFirst);
+toolbox.router.get('assets/*', toolbox.cacheFirst);
 toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
