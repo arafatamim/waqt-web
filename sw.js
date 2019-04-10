92bde22c8a1a30d9384e075ffb472aeb873cf55a
Tamim Arafat <tamim.arafat@gmail.com>
2019-04-11 02:18:25 +0600

    all done

--- sw.js
+++
@@ -1,4 +0,0 @@
-importScripts("sw-toolbox.js");
-toolbox.precache(["index.html", "styles/style.css", "scripts/script.js"]);
-toolbox.router.get("assets/*", toolbox.cacheFirst);
-toolbox.router.get("/*", toolbox.networkFirst, { networkTimeoutSeconds: 5 });
